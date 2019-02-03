eca02b3917bb4864f25c05d292543509e240da23
Hendi Santika <hendisantika@gmail.com>
2019-02-03 09:42:52 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmappedsupertype/SpringBootMappedSupertypeApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootmappedsupertype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMappedSupertypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMappedSupertypeApplication.class, args);
+    }
+
+}
+
